e2e47401a21e1d7b1f28aa2076570f2bb15a346e
nineSean <ninesean1989@gmail.com>
2021-04-28 12:34:19 +0800

    perf: 路由懒加载

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React, {FunctionComponent,} from "react"
+import React, {FunctionComponent, lazy, Suspense,} from "react"
 import ReactDOM from "react-dom"
 import {Provider} from "react-redux"
 import {Redirect, Switch,} from 'react-router-dom'
@@ -6,55 +6,60 @@ import Route from './components/Route'
 import {ConnectedRouter} from "connected-react-router"
 import store from './store'
 import history from './store/history'
-import Mine from './routes/Mine'
-import Home from './routes/Home'
-import Profile from './routes/Profile'
-import Register from './routes/Register'
-import Login from './routes/Login'
-import Detail from './routes/Detail'
-import Tabs from './components/Tabs'
 import './assets/styles/common.less'
 import './index.less'
 import './utils/flexiable'
 import zhCN from 'antd/lib/locale/zh_CN'
-import {ConfigProvider} from "antd"
+import {ConfigProvider, Spin} from "antd"
+import Tabs from './components/Tabs'
+
+const Mine = lazy(()=>import(/*webpackPrefetch:true*/'./routes/Mine'))
+const Home = lazy(()=>import(/*webpackPrefetch:true*/'./routes/Home'))
+const Profile = lazy(()=>import(/*webpackPrefetch:true*/'./routes/Profile'))
+const Register = lazy(()=>import(/*webpackPrefetch:true*/'./routes/Register'))
+const Login = lazy(()=>import(/*webpackPrefetch:true*/'./routes/Login'))
+const Detail = lazy(()=>import(/*webpackPrefetch:true*/'./routes/Detail'))
 
 const App: FunctionComponent = () => {
   return (
     <Provider store={store}>
       <ConnectedRouter history={history}>
         <ConfigProvider locale={zhCN}>
-          <main className="main">
-            <Switch>
-              <Route
-                path="/"
-                exact
-                component={Home}
-              />
-              <Route
-                path="/mine"
-                component={Mine}
-              />
-              <Route
-                path="/profile"
-                component={Profile}
-              />
-              <Route
-                path="/register"
-                component={Register}
-              />
-              <Route
-                path="/login"
-                component={Login}
-              />
-              <Route
-                path="/detail/:id"
-                component={Detail}
-              />
-              <Redirect to="/" />
-            </Switch>
-            <Tabs />
-          </main>
+          <Suspense
+            fallback={<Spin style={{margin: '30px auto', width: '100%'}}/>}
+          >
+            <main className="main">
+              <Switch>
+                <Route
+                  path="/"
+                  exact
+                  component={Home}
+                />
+                <Route
+                  path="/mine"
+                  component={Mine}
+                />
+                <Route
+                  path="/profile"
+                  component={Profile}
+                />
+                <Route
+                  path="/register"
+                  component={Register}
+                />
+                <Route
+                  path="/login"
+                  component={Login}
+                />
+                <Route
+                  path="/detail/:id"
+                  component={Detail}
+                />
+                <Redirect to="/" />
+              </Switch>
+              <Tabs />
+            </main>
+          </Suspense>
         </ConfigProvider>
       </ConnectedRouter>
     </Provider>
